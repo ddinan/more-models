b97a4de51963c63497d8fdeca8de326e14770e5c
VenkSociety <VenkSociety@users.noreply.github.com>
2018-03-17 13:26:12 +1000

    Rename plugin from "Cow Model" to "More Models"

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,11 +9,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CowModel")]
+[assembly: AssemblyTitle("More-Models")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CowModel")]
+[assembly: AssemblyProduct("More-Models")]
 [assembly: AssemblyCopyright("Copyright 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
