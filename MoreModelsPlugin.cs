ea716b559f4905cbd7682af0d812f8d5a1d66c58
VenkSociety <VenkSociety@users.noreply.github.com>
2018-07-08 21:12:35 +1000

    Fix typo from last commit
    Jeez Venk, I think you need to sleep.

--- MoreModelsPlugin.cs
+++ MoreModelsPlugin.cs
@@ -14,7 +14,7 @@ namespace MoreModelsPlugin {
 		public void Init(Game game) {
 			game.ModelCache.RegisterTextures("cow.png");
 			game.ModelCache.Register("cow", "cow.png", new CowModel(game));
-			game.ModelCache.Register("fly", "char.png", new FlyModel(game));
+			game.ModelCache.Register("fly", "char.png", new FlyingModel(game));
 			
 			// Recreate the modelcache VB to be bigger
 			game.Graphics.DeleteVb(ref game.ModelCache.vb);
