266b12de46cd44a5c0c174858b374f74373d47ae
Cheesse <andobso18@gmail.com>
2018-07-12 23:39:27 -0400

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿#region Using directives
+#region Using directives
 
 using System;
 using System.Reflection;
@@ -9,12 +9,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("More-Models")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("More Models")]
+[assembly: AssemblyDescription("Adds more player models to ClassicalSharp.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("More-Models")]
-[assembly: AssemblyCopyright("Copyright 2017")]
+[assembly: AssemblyProduct("More Models")]
+[assembly: AssemblyCopyright("Copyright 2018 by VenkSociety and Big C")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -28,4 +28,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.*")]
