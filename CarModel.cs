b37dcc698b1fe73df5178cace9b10fb001573cc6
Cheesse <andobso18@gmail.com>
2018-07-16 15:01:45 -0400

    Improve car model

--- CarModel.cs
+++ CarModel.cs
@@ -1,9 +1,12 @@
-﻿using ClassicalSharp.Entities;
+﻿using System;
+using ClassicalSharp;
+using ClassicalSharp.Entities;
 using ClassicalSharp.GraphicsAPI;
+using ClassicalSharp.Model;
 using ClassicalSharp.Physics;
 using OpenTK;
 
-namespace ClassicalSharp.Model
+namespace MoreModels
 {
     class CarModel : IModel 
     {
@@ -15,57 +18,65 @@ namespace ClassicalSharp.Model
 
         public override float GetEyeY(Entity e) { return 28f / 16f; }
 
-        public override Vector3 CollisionSize { get { return new Vector3(2.9f, 1.9f, 2.9f); } }
+        public override Vector3 CollisionSize { get { return new Vector3(4.75f, 2.125f, 4.75f); } }
 
         public override float NameYOffset { get { return 2.375f; } }
 
-        public override AABB PickingBounds { get { return new AABB(-1.5f, 0f, -1.5f, 1.5f, 2f, 1.5f); } }
+        public override AABB PickingBounds { get { return new AABB(-2.25f, 0f, -2.25f, 2.25f, 2.25f, 2.25f); } }
 
         public override void CreateParts() {
             vertices = new ModelVertex[boxVertices * boxesBuilt];
 
-            top        = BuildRotatedBox(MakeRotatedBoxBounds(-4, 16 + 4, -4, 4, 20 + 4, 4).TexOrigin(0, 0) .RotOrigin(0, 16 + 4, 0));
-            body       = BuildRotatedBox(MakeRotatedBoxBounds(-4, 0 + 4, -8, 4, 4 + 4, 8)  .TexOrigin(24, 0).RotOrigin(0, 0 + 4, 0));
-                
-            frontLeft  = BuildBox(MakeBoxBounds(-16 - 2, 2, -22, -12 - 2, 10, -14).TexOrigin(0, 12).RotOrigin(-16, 6, -18).Expand(2f));
-            frontRight = BuildBox(MakeBoxBounds(12 + 2, 2, -22, 16 + 2, 10, -14)  .TexOrigin(0, 12).RotOrigin(16, 6, -18) .Expand(2f));
-            backLeft   = BuildBox(MakeBoxBounds(-16 - 2, 2, 14, -12 - 2, 10, 22)  .TexOrigin(0, 12).RotOrigin(-16, 6, 18) .Expand(2f));
-            backRight  = BuildBox(MakeBoxBounds(12 + 2, 2, 14, 16 + 2, 10, 22)    .TexOrigin(0, 12).RotOrigin(16, 6, 18)  .Expand(2f));
+            top     = BuildRotatedBox(MakeRotatedBoxBounds(-20, 20, -16, 20, 36, 20).TexOrigin(120, 0));
+            body    = BuildRotatedBox(MakeRotatedBoxBounds(-22, 4, -40, 22, 20, 40) .TexOrigin(0, 0));
+            spoiler = BuildRotatedBox(MakeRotatedBoxBounds(-19, 27, 33, 19, 30, 42) .TexOrigin(120, 52));
+
+            tireFrontLeft  = BuildBox(MakeBoxBounds(-24, 0, -28, -20, 10, -18) .MirrorX().TexOrigin(156, 64).RotOrigin(-22, 5, -23));
+            tireBackLeft   = BuildBox(MakeBoxBounds(-24, 0, 18, -20, 10, 28)   .MirrorX().TexOrigin(156, 64).RotOrigin(-22, 5, 23));
+            mirrorLeft     = BuildBox(MakeBoxBounds(-27, 20, -15, -20, 25, -18).MirrorX().TexOrigin(202, 52).RotOrigin(-20, 22, -15));
+            spoilerLeft    = BuildBox(MakeBoxBounds(-15, 20, 31, -12, 30, 34)  .MirrorX().TexOrigin(184, 64).RotOrigin(-12, 20, 31));
+
+            tireFrontRight = BuildBox(MakeBoxBounds(20, 0, -28, 24, 10, -18) .TexOrigin(156, 64).RotOrigin(22, 5, -23));
+            tireBackRight  = BuildBox(MakeBoxBounds(20, 0, 18, 24, 10, 28)   .TexOrigin(156, 64).RotOrigin(22, 5, 23));
+            mirrorRight    = BuildBox(MakeBoxBounds(20, 20, -15, 27, 25, -18).TexOrigin(202, 52).RotOrigin(20, 22, -15));
+            spoilerRight   = BuildBox(MakeBoxBounds(12, 20, 31, 15, 30, 34)  .TexOrigin(184, 64).RotOrigin(12, 20, 31));
+
+            frontLeft = BuildBox(MakeBoxBounds(-22, 4, -31, -20, 14, -15).MirrorX().TexOrigin(120, 64));
+            backLeft  = BuildBox(MakeBoxBounds(-22, 4, 15, -20, 14, 31)  .MirrorX().TexOrigin(120, 64));
+
+            frontRight = BuildBox(MakeBoxBounds(20, 4, -31, 22, 14, -15).TexOrigin(120, 64));
+            backRight  = BuildBox(MakeBoxBounds(20, 4, 15, 22, 14, 31)  .TexOrigin(120, 64));            
         }
 
         public override void DrawModel(Entity p)
         {
+            uScale = 1f / 256f;
+            vScale = 1f / 128f;
+
             game.Graphics.BindTexture(GetTexture(p));
 
-            DrawScale(4f, top);
-            DrawScale(4f, body);
+            DrawPart(top);
+            DrawPart(body);
+            DrawPart(frontLeft);
+            DrawPart(frontRight);
+            DrawPart(backLeft);
+            DrawPart(backRight);
+            DrawPart(spoiler);
 
-            DrawRotate(-p.anim.walkTime, 0f, 0f, frontLeft, false);
-            DrawRotate(-p.anim.walkTime, 0f, 0f, frontRight, false);
-            DrawRotate(-p.anim.walkTime, 0f, 0f, backLeft, false);
-            DrawRotate(-p.anim.walkTime, 0f, 0f, backRight, false);
+            DrawRotate(-p.anim.walkTime, 0f, 0f, tireFrontLeft, false);
+            DrawRotate(-p.anim.walkTime, 0f, 0f, tireFrontRight, false);
+            DrawRotate(-p.anim.walkTime, 0f, 0f, tireBackLeft, false);
+            DrawRotate(-p.anim.walkTime, 0f, 0f, tireBackRight, false);
+            DrawRotate(0f, (float)Math.PI / 12f, 0f, mirrorLeft, false);
+            DrawRotate(0f, (float)Math.PI / -12f, 0f, mirrorRight, false);
+            DrawRotate((float)Math.PI / 6f, 0f, 0f, spoilerLeft, false);
+            DrawRotate((float)Math.PI / 6f, 0f, 0f, spoilerRight, false);
 
             UpdateVB();
         }
 
-        protected void DrawScale(float scale, ModelPart part)
-        {
-            VertexP3fT2fC4b vertex = default(VertexP3fT2fC4b);
-            VertexP3fT2fC4b[] finVertices = game.ModelCache.vertices;
+        private ModelPart body, top, tireFrontLeft, tireFrontRight, tireBackLeft, tireBackRight, frontLeft, frontRight, backLeft, backRight, mirrorLeft, mirrorRight, spoilerLeft, spoilerRight, spoiler;
 
-            for (int i = 0; i < part.Count; i++)
-            {
-                ModelVertex v = vertices[part.Offset + i];
-                vertex.X = (v.X - part.RotX) * scale + part.RotX; vertex.Y = (v.Y - part.RotY) * scale + part.RotY; vertex.Z = (v.Z - part.RotZ) * scale + part.RotZ;
-                vertex.Col = cols[i >> 2];
-
-                vertex.U = (v.U & UVMask) * uScale - (v.U >> UVMaxShift) * 0.01f * uScale;
-                vertex.V = (v.V & UVMask) * vScale - (v.V >> UVMaxShift) * 0.01f * vScale;
-                finVertices[index++] = vertex;
-            }
-        }
-        private ModelPart body, top, frontLeft, frontRight, backLeft, backRight;
-
-        private const int boxesBuilt = 6;
+        private const int boxesBuilt = 15;
     }
 }
