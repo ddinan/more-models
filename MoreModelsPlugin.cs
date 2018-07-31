b0f8f840a68f8588b625cadc0bf0e66fb6b8724f
VenkSociety <VenkSociety@users.noreply.github.com>
2018-07-31 17:43:02 +1000

    Add cavespider, husk, stray, witherskeleton and zombiepigman models

--- MoreModelsPlugin.cs
+++ MoreModelsPlugin.cs
@@ -13,26 +13,36 @@ namespace MoreModels
 		public void Init(Game game)
         {
             game.ModelCache.RegisterTextures("car.png");
+            game.ModelCache.RegisterTextures("cave_spider.png");
             game.ModelCache.RegisterTextures("cow.png");
             game.ModelCache.RegisterTextures("croc.png");
             game.ModelCache.RegisterTextures("enderman.png");
+            game.ModelCache.RegisterTextures("husk.png");
             game.ModelCache.RegisterTextures("printer.png");
+            game.ModelCache.RegisterTextures("stray.png");
+            game.ModelCache.RegisterTextures("wither_skeleton.png");
+            game.ModelCache.RegisterTextures("zombie_pigman.png");
 
             game.ModelCache.Register("car", "car.png", new CarModel(game));
+            game.ModelCache.Register("cavespider", "cave_spider.png", new CaveSpiderModel(game));
 			game.ModelCache.Register("cow", "cow.png", new CowModel(game));
             game.ModelCache.Register("croc", "croc.png", new CrocModel(game));
             game.ModelCache.Register("enderman", "enderman.png", new EndermanModel(game));
 			game.ModelCache.Register("flying", "char.png", new FlyingModel(game));
 			game.ModelCache.Register("headless", "char.png", new HeadlessModel(game));
+			game.ModelCache.Register("husk", "husk.png", new HuskModel(game));
 			game.ModelCache.Register("holding", "char.png", new HoldingModel(game));
             game.ModelCache.Register("male", "char.png", new MaleModel(game));
             game.ModelCache.Register("printer", "printer.png", new PrinterModel(game));
+            game.ModelCache.Register("stray", "stray.png", new StrayModel(game));
+            game.ModelCache.Register("witherskeleton", "wither_skeleton.png", new WitherSkeletonModel(game));
+            game.ModelCache.Register("zombiepigman", "zombie_pigman.png", new ZombiePigmanModel(game));
 			
 			// Recreate the modelcache VB to be bigger
 			game.Graphics.DeleteVb(ref game.ModelCache.vb);
 			game.ModelCache.vertices = new VertexP3fT2fC4b[24 * 20];
 			game.ModelCache.vb = game.Graphics.CreateDynamicVb(VertexFormat.P3fT2fC4b, game.ModelCache.vertices.Length);
-			game.Server.AppName += " + More Models v1.0";
+			game.Server.AppName += " + More Models v1.0.1";
 		}
 		
 		public void Ready(Game game) { }
