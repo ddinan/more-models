1179b07c6fddfa74c230b292bfe8e4e7f9db29fc
VenkSociety <VenkSociety@users.noreply.github.com>
2018-08-05 15:44:46 +1000

    Add magma cube and slime models

---
+++ Models/MagmaCubeModel.cs
@@ -0,0 +1,37 @@
+﻿using ClassicalSharp;
+using ClassicalSharp.Entities;
+using ClassicalSharp.Model;
+using ClassicalSharp.Physics;
+using OpenTK;
+
+namespace MoreModels {
+	public class MagmaCubeModel : IModel {
+		public MagmaCubeModel(Game game) : base(game) { SurivalScore = 16; }
+
+		public override void CreateParts() {
+			vertices = new ModelVertex[boxVertices * boxesBuilt];
+			
+			head     = BuildBox(MakeBoxBounds(4, 8, -4, -4, 0, 4).TexOrigin(0, 0).RotOrigin(0, 0, 0));
+        }
+
+		public override float NameYOffset { get { return 0.5f; } }
+
+		public override float GetEyeY(Entity entity) { return 6f/16f; }
+
+		public override Vector3 CollisionSize { get { return new Vector3(0.875f, 0.875f, 0.875f); } }
+
+		public override AABB PickingBounds { get { return new AABB(-5f / 16f, 0f, -0.875f, 5f / 16f, 1f, 9f / 16f); }	}
+
+		public override void DrawModel(Entity p) {
+			ApplyTexture(p);
+
+            DrawRotate(-p.HeadXRadians, 0f, 0f, head, true);
+
+			UpdateVB();
+		}
+
+		private ModelPart head;
+
+        private const int boxesBuilt = 1;
+	}
+}
--- MoreModelsPlugin.cs
+++ MoreModelsPlugin.cs
@@ -6,7 +6,7 @@ namespace MoreModels
 {
 	public sealed class Core : Plugin {
 		
-		public string ClientVersion { get { return "0.99.4"; } }
+		public string ClientVersion { get { return "0.99.*"; } }
 		
 		public void Dispose() { }
 		
@@ -19,23 +19,27 @@ namespace MoreModels
             game.ModelCache.RegisterTextures("enderman.png");
             game.ModelCache.RegisterTextures("enderman_eyes.png");
             game.ModelCache.RegisterTextures("husk.png");
+            game.ModelCache.RegisterTextures("magmacube.png");
             game.ModelCache.RegisterTextures("male.png");
             game.ModelCache.RegisterTextures("printer.png");
+            game.ModelCache.RegisterTextures("slime.png");
             game.ModelCache.RegisterTextures("stray.png");
             game.ModelCache.RegisterTextures("wither_skeleton.png");
             game.ModelCache.RegisterTextures("zombie_pigman.png");
 
             game.ModelCache.Register("car", "car.png", new CarModel(game));
             game.ModelCache.Register("cavespider", "cave_spider.png", new CaveSpiderModel(game));
-			game.ModelCache.Register("cow", "cow.png", new CowModel(game));
+			      game.ModelCache.Register("cow", "cow.png", new CowModel(game));
             game.ModelCache.Register("croc", "croc.png", new CrocModel(game));
             game.ModelCache.Register("enderman", "enderman.png", new EndermanModel(game));
-			game.ModelCache.Register("flying", "char.png", new FlyingModel(game));
-			game.ModelCache.Register("headless", "char.png", new HeadlessModel(game));
-			game.ModelCache.Register("husk", "husk.png", new HuskModel(game));
-			game.ModelCache.Register("holding", "char.png", new HoldingModel(game));
+			      game.ModelCache.Register("flying", "char.png", new FlyingModel(game));
+			      game.ModelCache.Register("headless", "char.png", new HeadlessModel(game));
+			      game.ModelCache.Register("husk", "husk.png", new HuskModel(game));
+			      game.ModelCache.Register("holding", "char.png", new HoldingModel(game));
             game.ModelCache.Register("male", "male.png", new MaleModel(game));
+            game.ModelCache.Register("magmacube", "magmacube.png", new MagmaCubeModel(game));
             game.ModelCache.Register("printer", "printer.png", new PrinterModel(game));
+            game.ModelCache.Register("slime", "slime.png", new SlimeModel(game));
             game.ModelCache.Register("stray", "stray.png", new StrayModel(game));
             game.ModelCache.Register("witherskeleton", "wither_skeleton.png", new WitherSkeletonModel(game));
             game.ModelCache.Register("zombiepigman", "zombie_pigman.png", new ZombiePigmanModel(game));
@@ -44,7 +48,7 @@ namespace MoreModels
 			game.Graphics.DeleteVb(ref game.ModelCache.vb);
 			game.ModelCache.vertices = new VertexP3fT2fC4b[24 * 20];
 			game.ModelCache.vb = game.Graphics.CreateDynamicVb(VertexFormat.P3fT2fC4b, game.ModelCache.vertices.Length);
-			game.Server.AppName += " + More Models v1.0.1";
+			game.Server.AppName += " + More Models v1.1";
 		}
 		
 		public void Ready(Game game) { }
